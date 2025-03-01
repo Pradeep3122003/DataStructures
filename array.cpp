3ecc707ecea95fe5ca3bc52ac2745ef0843a3c8c
Nagesh <kn734917@gmail.com>
2025-03-01 21:22:25 +0530

    updated

--- array.cpp
+++ array.cpp
@@ -1,73 +1,95 @@
 #include <iostream>
 using namespace std;
 
-void insertionSort(int arr[], int n) {
-    for (int i = 1; i < n; ++i) {
-        int key = arr[i];
-        int j = i - 1;
-        while (j >= 0 && arr[j] > key) {
-            arr[j + 1] = arr[j];
-            j = j - 1;
+class Array {
+public:
+    int arr[100];
+    int size;
+
+
+    Array() {
+        size = 0;
+    }
+
+    void insertionSort() {
+        for (int i = 1; i < size; ++i) {
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
         }
-        arr[j + 1] = key;
-    }
-}
-
-void printArray(int arr[], int n) {
-    for (int i = 0; i < n; ++i)
-        cout << arr[i] << " ";
-    cout << endl;
-}
-
-void insertElement(int arr[], int &n, int element, int position) {
-    if (position < 0 || position > n) {
-        cout << "Invalid position!" << endl;
-        return;
     }
 
-    for (int i = n; i > position; i--) {
-        arr[i] = arr[i - 1];
+    void printArray() {
+        for (int i = 0; i < size; ++i)
+            cout << arr[i] << " ";
+        cout << endl;
     }
 
-    arr[position] = element;
-    n++;
+    void insertElement(int element, int position) {
+        if (position < 0 || position > size) {
+            cout << "Invalid position!" << endl;
+            return;
+        }
 
-    insertionSort(arr, n);
-}
+        if (size >= 100) {
+            cout << "Array is full!" << endl;
+            return;
+        }
 
-void deleteElement(int arr[], int &n, int position) {
-    if (position < 0 || position >= n) {
-        cout << "Invalid position!" << endl;
-        return;
+        for (int i = size; i > position; i--) {
+            arr[i] = arr[i - 1];
+        }
+
+        arr[position] = element;
+        size++;
+
+        insertionSort();
     }
 
-    for (int i = position; i < n - 1; i++) {
-        arr[i] = arr[i + 1];
+    void deleteElement(int position) {
+        if (position < 0 || position >= size) {
+            cout << "Invalid position!" << endl;
+            return;
+        }
+
+        for (int i = position; i < size - 1; i++) {
+            arr[i] = arr[i + 1];
+        }
+        size--;
     }
-    n--;
-}
+
+    void inputElements() {
+        cout << "Enter the number of initial elements: ";
+        cin >> size;
+
+        if (size > 100) {
+            cout << "Too many elements! Using first 100." << endl;
+            size = 100;
+        }
+
+        cout << "Enter " << size << " elements: ";
+        for (int i = 0; i < size; i++) {
+            cin >> arr[i];
+        }
+
+        insertionSort();
+    }
+
+    void displayInitialArray() {
+        cout << "Initial sorted array: ";
+        printArray();
+    }
+};
 
 int main() {
-    int arr[100];
-    int n;
+    Array obj;
 
-    cout << "Enter the number of initial elements: ";
-    cin >> n;
-
-    if (n > 100) {
-        cout << "Too many elements! Using first 100." << endl;
-        n = 100;
-    }
-
-    cout << "Enter " << n << " elements: ";
-    for (int i = 0; i < n; i++) {
-        cin >> arr[i];
-    }
-
-    insertionSort(arr, n);
-
-    cout << "Initial sorted array: ";
-    printArray(arr, n);
+    obj.inputElements();
+    obj.displayInitialArray();
 
     while (true) {
         cout << "\nMenu:\n1. Insert Element\n2. Delete Element\n3. Display Array\n4. Exit\n";
@@ -83,16 +105,16 @@ int main() {
                 cout << "Enter the position: ";
                 int position;
                 cin >> position;
-                insertElement(arr, n, element, position);
+                obj.insertElement(element, position);
                 break;
             case 2:
                 cout << "Enter the position to delete: ";
                 cin >> position;
-                deleteElement(arr, n, position);
+                obj.deleteElement(position);
                 break;
             case 3:
                 cout << "Current array: ";
-                printArray(arr, n);
+                obj.printArray();
                 break;
             case 4:
                 return 0;
