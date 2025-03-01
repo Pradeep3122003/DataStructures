ac228ac99f944dddcacab391020d66795b4236bc
Pradeep3122003 <pradeepkh312@gmail.com>
2025-03-01 13:05:49 +0530

    descriptions added

--- main.cpp
+++ main.cpp
@@ -22,26 +22,45 @@ cout<<"root/"<<a[i]<<">";
 }
 
 
-string description(int i){ 
-string a[5]={ "Arrays are used to store multiple values in a single variable, instead of declaring separate variables for each value.", 
+void description(int i){ 
+string a[6]={ "","Arrays are used to store multiple values in a single variable, instead of declaring separate variables for each value.", 
 "A stack stores multiple elements in a specific order, called LIFO.\nLIFO stands for Last in, First Out. To vizualise LIFO, think of a pile of pancakes, where pancakes are both added and removed from the top. So when removing a pancake, it will always be the last one you added. This way of organizing elements is called LIFO in computer science and programming",
 "A queue stores multiple elements in a specific order, called FIFO. FIFO stands for First in, First Out. To visualize FIFO, think of a queue as people standing in line in a supermarket. The first person to stand in line is also the first who can pay and leave the supermarket. This way of organizing elements is called FIFO in computer science and programming",
 "The Tree data structure is similar to Linked Lists in that each node contains data and can be linked to other nodes.",
 "A Graph is a non-linear data structure that consists of vertices (nodes) and edges."
 
 };
-return "hi";
+cout<<a[i]<<endl;
 
 }
 
 int check1(string a)
 {
 
-string s[5]={"help","array", "queue", "create", "use"};
-for(int i=0;i<5;i++)
+string s[6]={"help","array", "stack", "queue", "tree", "graph"};
+for(int i=0;i<6;i++)
 {
  if(s[i] == a)
 {
+ direct=i;
+return 1;
+}
+
+}
+return 0;
+
+}
+
+
+int check2(string a, string b)
+{
+
+string s[6]={"help","array", "stack", "queue", "tree", "graph"};
+for(int i=0;i<6;i++)
+{
+ if(s[i] == b)
+{
+ description(i);
 return 1;
 }
 
@@ -52,7 +71,7 @@ return 0;
 
 string inputvalidate(string a, int s){
 string frag[5], temp;
-int c=0, c1=0;
+int c=0;
 if(a=="")
 {return "";}
 for(int i=0;i <= s+1;i++){
@@ -76,11 +95,17 @@ if(c==1)
   return "Invalid command";
 
 }
-else{
- return "valid command";
 }
+else if(c==2){
+
+if(check2(frag[0], frag[1])==0){
+   return "Invalid command";}
 }
-return "Enter command";
+
+
+
+
+return "";
 }
 
 int main()
