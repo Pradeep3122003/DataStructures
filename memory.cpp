35a803e78bc845ee2e6766942a50247f53e1f5b6
Pradeep3122003 <pradeepkh312@gmail.com>
2025-03-03 13:56:54 +0530

    array creation memory ready

--- memory.cpp
+++ memory.cpp
@@ -4,37 +4,55 @@ using namespace std;
 class memory{
 
     public:
-    string arrayname[5]= {"a","b","c","d"};
+    int namecount[5]={0,0,0,0,0};
+    string arrayname[5];
     string queuename[5];
     string stackname[5];
     string treename[5];
     string graphname[5];
+
+//array operations completed
 int arraycreation(string g)
 {
- for(int i=0;i<5;i++)
+ if(namecount[0]==0)
+{
+arrayname[namecount[0]]=g;
+namecount[0]+=1;
+ cout<<"Array '"<<g<<"' Created.";
+return 1;
+}
+
+for(int i=0;i<=namecount[0];i++)
  {
    if(g==arrayname[i]){
 cout<<"array already exist";
 return 0;
-}
- for(int i=0;i<5;i++)
+}}
+
+if(namecount[0]==5)
 {
-if(g!=arrayname[i])
-{
-int n;
-arrayname[i]=g[i];
-}
-}
-}
+cout<<"Maximum 5 arrays can be created";
 return 0;
-}
+} else {
+arrayname[namecount[0]]=g;
+namecount[0]+=1;
+cout<<"Array '"<<g<<"' Created.";
+return 0;
+}}
 };
+
+
+
+
 int main()
 {
-memory obj;
-string words;
-cout<<"enter the command ";
-cin>>words;
-obj.arraycreation(words);
+memory user;
+string word;
+while(true){
+cout<<"\nName: ";
+fflush(stdin);
+cin>>word;
+user.arraycreation(word);
+}
 return 0;
 }
