9a20387a8448961071fb6a574ba32577fbb3d35f
Pradeep3122003 <pradeepkh312@gmail.com>
2025-03-03 14:06:55 +0530

    all ds creation added

--- memory.cpp
+++ memory.cpp
@@ -39,6 +39,129 @@ namecount[0]+=1;
 cout<<"Array '"<<g<<"' Created.";
 return 0;
 }}
+
+
+//queue operations completed
+
+int queuecreation(string g)
+{
+ if(namecount[1]==0)
+{
+queuename[namecount[1]]=g;
+namecount[1]+=1;
+ cout<<"Queue '"<<g<<"' Created.";
+return 1;
+}
+
+for(int i=0;i<=namecount[1];i++)
+ {
+   if(g==queuename[i]){
+cout<<"queue already exist";
+return 0;
+}}
+
+if(namecount[1]==5)
+{
+cout<<"Maximum 5 queues can be created";
+return 0;
+} else {
+queuename[namecount[1]]=g;
+namecount[1]+=1;
+cout<<"Queue '"<<g<<"' Created.";
+return 0;
+}}
+
+
+//stack operations completed
+
+int stackcreation(string g)
+{
+ if(namecount[2]==0)
+{
+stackname[namecount[2]]=g;
+namecount[2]+=1;
+ cout<<"Stack '"<<g<<"' Created.";
+return 1;
+}
+
+for(int i=0;i<=namecount[2];i++)
+ {
+   if(g==stackname[i]){
+cout<<"stack already exist";
+return 0;
+}}
+
+if(namecount[2]==5)
+{
+cout<<"Maximum 5 stacks can be created";
+return 0;
+} else {
+stackname[namecount[2]]=g;
+namecount[2]+=1;
+cout<<"Stack '"<<g<<"' Created.";
+return 0;
+}}
+
+//tree operations completed
+
+int treecreation(string g)
+{
+ if(namecount[3]==0)
+{
+treename[namecount[3]]=g;
+namecount[3]+=1;
+ cout<<"Tree '"<<g<<"' Created.";
+return 1;
+}
+
+for(int i=0;i<=namecount[3];i++)
+ {
+   if(g==treename[i]){
+cout<<"tree already exist";
+return 0;
+}}
+
+if(namecount[3]==5)
+{
+cout<<"Maximum 5 trees can be created";
+return 0;
+} else {
+treename[namecount[3]]=g;
+namecount[3]+=1;
+cout<<"Tree '"<<g<<"' Created.";
+return 0;
+}}
+
+//graph operations completed
+
+int graphcreation(string g)
+{
+ if(namecount[4]==0)
+{
+graphname[namecount[4]]=g;
+namecount[4]+=1;
+ cout<<"Graph '"<<g<<"' Created.";
+return 1;
+}
+
+for(int i=0;i<=namecount[4];i++)
+ {
+   if(g==graphname[i]){
+cout<<"graph already exist";
+return 0;
+}}
+
+if(namecount[4]==5)
+{
+cout<<"Maximum 5 graphs can be created";
+return 0;
+} else {
+graphname[namecount[4]]=g;
+namecount[4]+=1;
+cout<<"Graph '"<<g<<"' Created.";
+return 0;
+}}
+
 };
 
 
